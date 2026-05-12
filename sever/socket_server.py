024ccefed133af9d7d23a56fbfaf3b211489dbee
Nimna-Kumara <nimnaanjana2001@gmail.com>
2026-05-12 23:42:01 +0530

    socket_server.py line 19: print() added

--- sever/socket_server.py
+++ sever/socket_server.py
@@ -10,16 +10,20 @@ class HTTPServer:
 
     def _create_socket(self) -> socket.socket:
 
+        # AF_INET  -> IPv4
+        # SOCK_STREAM -> TCP
         sever_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sever_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sever_socket.bind((self.host, self.host))
         sever_socket.listen(settings.BACKLOG)
+        print(f"[SERVER] Listening on {self.host}:{self.port}")
 
         return sever_socket
 
 
     def start(self):
         # loop: sock.accept() → submit client to thread pool
+
         pass
 
     def _handle_client(self, conn: socket.socket, addr: tuple):
