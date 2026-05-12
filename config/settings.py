a6b812bc5486dc8e513ccbcd39f4a18590d3a99f
Nimna-Kumara <nimnaanjana2001@gmail.com>
2026-05-12 22:51:26 +0530

    Initial Commit

---
+++ config/settings.py
@@ -0,0 +1,6 @@
+HOST = '127.0.0.1'
+PORT = 8080
+BACKLOG = 5             # max queued connections
+BUFFER_SIZE = 4096      # bytes read per recv() call
+MAX_WORKERS = 10        # thread pool size
+
---
+++ sever/socket_server.py
@@ -0,0 +1,31 @@
+import socket
+
+from config import settings 
+
+class HTTPServer:
+    def __init__(self, host=settings.HOST, port=settings.PORT):
+        self.host = host
+        self.port = port
+
+
+    def _create_socket(self) -> socket.socket:
+
+        sever_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sever_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sever_socket.bind((self.host, self.host))
+        sever_socket.listen(settings.BACKLOG)
+
+        return sever_socket
+
+
+    def start(self):
+        # loop: sock.accept() → submit client to thread pool
+        pass
+
+    def _handle_client(self, conn: socket.socket, addr: tuple):
+        # recv raw bytes → parse → route → send response → close
+        pass
+
+    def stop(self):
+       # set stop event, close server socket
+       pass 
